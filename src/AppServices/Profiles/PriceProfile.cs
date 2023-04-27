7cb79431674085427aa4e2b15183665adfa46f8c
Claudinei José Santos <claudineibitta@gmail.com>
2023-04-27 20:05:31 -0300

    feat: adicionando profiles para os recursos

---
+++ src/AppServices/Profiles/PriceProfile.cs
@@ -0,0 +1,20 @@
+﻿using Application.Models.Request.Price;
+using Application.Models.Response;
+using AutoMapper;
+using DomainModels.Entities;
+
+namespace AppServices.Profiles
+{
+    public class PriceProfile : Profile
+    {
+        public PriceProfile()
+        {
+            CreateMap<CreatePriceRequest, Price>();
+            CreateMap<UpdatePriceRequest, Price>().ReverseMap();
+            CreateMap<Price, PriceResponse>()
+                .ForMember(x => x.InitialTerm, opts => opts.MapFrom(x => x.InitialTerm))
+                .ForMember(x => x.FinalTerm, opts => opts.MapFrom(x => x.FinalTerm))
+                .ForMember(x => x.CurrentValue, opts => opts.MapFrom(x => x.CurrentValue));
+        }
+    }
+}
