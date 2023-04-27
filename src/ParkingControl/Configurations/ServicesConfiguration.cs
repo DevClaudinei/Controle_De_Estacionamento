c59fad2abcf8a72951a2daf13ce262d512797372
Claudinei José Santos <claudineibitta@gmail.com>
2023-04-27 20:25:40 -0300

    feat: adicionando configuracoes e injecoes de dependencia

---
+++ src/ParkingControl/Configurations/ServicesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AppServices.Services;
+using AppServices.Services.Interfaces;
+using DomainServices.Services;
+using DomainServices.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ParkingControl.Configurations;
+
+public static class ServicesConfiguration
+{
+    public static void AddServicesConfiguration(this IServiceCollection services)
+    {
+        services.AddTransient<IParkingAppService, ParkingAppService>();
+
+        services.AddTransient<IParkingService, ParkingService>();
+
+        services.AddTransient<IPriceAppService, PriceAppService>();
+
+        services.AddTransient<IPriceService, PriceService>();
+    }
+}
