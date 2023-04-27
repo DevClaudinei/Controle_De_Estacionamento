3a2e01fe79f8e6d719b8ab7bcad3d997e28ff7b0
Claudinei José Santos <claudineibitta@gmail.com>
2023-04-27 20:23:48 -0300

    feat: adicionando migrations referente aos contextos

---
+++ src/Infrastructure.Data/Migrations/Data/20230427231642_inserindo_tabelas_no_banco.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations.Data
+{
+    public partial class inserindo_tabelas_no_banco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Price",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    InitialTerm = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    FinalTerm = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CurrentValue = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Price", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parking",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Plate = table.Column<string>(type: "TEXT", nullable: false),
+                    ArrivalTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DepartureTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ParkingTime = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    TimeToBeBilled = table.Column<int>(type: "INTEGER", nullable: false),
+                    AmountToPay = table.Column<double>(type: "REAL", nullable: false),
+                    PriceId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parking", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Parking_Price_PriceId",
+                        column: x => x.PriceId,
+                        principalTable: "Price",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parking_PriceId",
+                table: "Parking",
+                column: "PriceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parking");
+
+            migrationBuilder.DropTable(
+                name: "Price");
+        }
+    }
+}
