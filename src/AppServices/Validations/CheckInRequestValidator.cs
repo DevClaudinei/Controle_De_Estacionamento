6464887e28014e56ab139a7a856ce43e309d3bb2
Claudinei José Santos <claudineibitta@gmail.com>
2023-04-27 20:06:58 -0300

    feat: adicionando validador de propriedades

---
+++ src/AppServices/Validations/CheckInRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.Models.Request.Parking;
+using FluentValidation;
+
+namespace AppServices.Validations
+{
+    public class CheckInRequestValidator : AbstractValidator<CheckInRequest>
+    {
+        public CheckInRequestValidator() 
+        {
+            RuleFor(x => x.Plate)
+                .NotEmpty()
+                .NotNull()
+                .Length(8)
+                .Must(x => x.LicensePlateIsValid())
+                .WithMessage("License plate must be in the format ABC-1234 or ABC-7A45");
+        }
+    }
+}
---
+++ src/AppServices/Validations/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AppServices.Validations
+{
+    public static class StringExtensions
+    {
+        public static bool LicensePlateIsValid(this string plate)
+        {
+            var expression = "[A-z]{3}-\\d[A-j0-9]\\d{2}";
+            return Regex.Match(plate, expression).Success;
+        }
+    }
+}
