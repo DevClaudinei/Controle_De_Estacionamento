748e54e4cc2ce3dd14629d4f2aca469fbe3238ef
Claudinei José Santos <claudineibitta@gmail.com>
2023-04-27 20:38:18 -0300

    feat: adicionando models e views

---
+++ src/ParkingControl/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ParkingControl.Models;
+
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
