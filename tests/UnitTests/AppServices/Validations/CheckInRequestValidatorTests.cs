dcd1bad80e84d9ad910f17bef229968edc5acdc5
Claudinei José Santos <claudineibitta@gmail.com>
2023-05-02 21:17:22 -0300

    feat: criacao de testes para camada de aplicacao

---
+++ tests/UnitTests/AppServices/Validations/CheckInRequestValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using AppServices.Validations;
+using FluentValidation.TestHelper;
+using UnitTests.Fixtures;
+
+namespace UnitTests.AppServices.Validations
+{
+    public class CheckInRequestValidatorTests
+    {
+        private readonly CheckInRequestValidator _checkInRequestValidator = new();
+
+        [Fact]
+        public void Should_Pass_When_Execute_CheckInRequestValidator()
+        {
+            // Arrange
+            var checkInRequestFake = CheckInRequestFixture.CheckInRequestFake();
+            checkInRequestFake.Plate = "ABC-1J23";
+
+            // Act
+            var result = _checkInRequestValidator.TestValidate(checkInRequestFake);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Should_Fail_When_Execute_CheckInRequestValidator()
+        {
+            // Arrange
+            var checkInRequestFake = CheckInRequestFixture.CheckInRequestFake();
+
+            // Act
+            var result = _checkInRequestValidator.TestValidate(checkInRequestFake);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Plate);
+        }
+    }
+}
